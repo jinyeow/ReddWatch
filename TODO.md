340f40179074385fbf91bb3cb03d203fbd3839e7
jinyeow <jin-yeow@outlook.com>
2017-10-29 22:43:06 +1100

    updated TODO.md. added some options to optparse in bin/reddwatch. added some empty methods into lib/reddwatch.rb for the interface (note: module methods require self.method_name)

--- TODO.md
+++ TODO.md
@@ -1,6 +1,11 @@
 # TODO
 
+Updated at: 22:41 29/10/17
+
 * Add Guardfile
+* Flesh out bin/reddwatch
+* Implement interface for lib/reddwatch
 * add wrapper for Redd
+* add class for notifier
 * Send me an e-mail at the start/end of each day/week of the most popular posts
 * Add how to get client-id/secret to README.md to get the gem working.
--- lib/reddwatch.rb
+++ lib/reddwatch.rb
@@ -1,60 +1,71 @@
 require "reddwatch/version"
 
 require 'dotenv/load'
+require 'pp'
+
+require 'forwardable'
 
 require 'redd'
 require 'gir_ffi'
 
+# @see Reddwatch::ClassMethods
 module Reddwatch
+  extend Forwardable
 
   # NOTE: The following is only a rough outline of what I want the gem to do.
   # Needs to be run on a loop with more options.
 
-  # Initialize Redd
-  session = Redd.it(
-    client_id: ENV['CLIENT_ID'],
-    secret:    ENV['CLIENT_SECRET']
-  )
+  def self.init(opts)
+    pp opts
+  end
 
-  appname   = 'ReddWatch'
-  subreddit = 'ruby'
+  def example
+    # Initialize Redd
+    session = Redd.it(
+      client_id: ENV['CLIENT_ID'],
+      secret:    ENV['CLIENT_SECRET']
+    )
 
-  # Get new posts from subreddit
-  new = session.subreddit(subreddit).new
-  posts = new.children
+    appname   = 'ReddWatch'
+    subreddit = 'ruby'
 
-  # Notification init
-  GirFFI.setup :Notify
-  Notify.init(appname)
+    # Get new posts from subreddit
+    new = session.subreddit(subreddit).new
+    posts = new.children
 
-  # Send desktop notification
-  Notify::Notification.new(
-    appname,
-    "#{posts.first.title}: #{posts.first.url}",
-    "dialog-info"
-  ).show
+    # Notification init
+    GirFFI.setup :Notify
+    Notify.init(appname)
 
-  # posts.each do |post|
-  #   Notify::Notification.new(
-  #     "#{appname} - #{subreddit}",
-  #     "#{post.title}\n\n#{post.url}",
-  #     "dialog-info"
-  #   )
-  # end
+    # Send desktop notification
+    Notify::Notification.new(
+      appname,
+      "#{posts.first.title}: #{posts.first.url}",
+      "dialog-info"
+    ).show
+
+    # posts.each do |post|
+    #   Notify::Notification.new(
+    #     "#{appname} - #{subreddit}",
+    #     "#{post.title}\n\n#{post.url}",
+    #     "dialog-info"
+    #   )
+    # end
+  end
 
   #
   # Main interface
   #
 
-  def start
+  def self.start
+    puts "Starting Reddwatch..."
   end
 
-  def stop
+  def self.stop
+    puts "Stopping Reddwatch..."
   end
 
-  def status
-  end
-
-  def help
+  def self.status
+    puts "In progress..."
   end
 end
