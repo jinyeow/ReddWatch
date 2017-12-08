446af8f31fa3ec7dde205b222835c8d30347b49c
jinyeow <jin-yeow@outlook.com>
2017-12-08 17:13:08 +1100

    reddwatch will show only 5 of the newest posts on the first run instead of all of them. on subsequent runs it will only show the newest based on last_checked variable. Added some basic logging in the Processor::Base module for starts/stops events and errors.

--- TODO.md
+++ TODO.md
@@ -1,9 +1,8 @@
 # TODO
 
-Updated at: 21:29 06/12/17
+Updated at: 17:08 08/12/17
 
-* Add Guardfile
-* Flesh out bin/reddwatch
-* Implement interface for lib/reddwatch
+* Create a Utility::Logger module to log events and errors to /tmp/reddwatch.log
+* Implement 'list' related functionality
 * Send me an e-mail at the start/end of each day/week of the most popular posts
 * Add how to get client-id/secret to README.md to get the gem working.
--- lib/reddwatch/processor/base.rb
+++ lib/reddwatch/processor/base.rb
@@ -1,30 +1,59 @@
 require 'reddwatch'
 
+# Monkey Patch some time-related methods to Integer
+# e.g. 5.seconds, 5.minutes, 5.hours # => returns the amount of seconds
+Integer.class_eval {
+  define_method :seconds do
+    return self
+  end
+
+  define_method :minutes do
+    return 60 * self
+  end
+
+  define_method :hours do
+    return 60 * 60 * self
+  end
+}
+
 module Reddwatch
   module Processor
     class Base
+      DEFAULT_WAIT_INTERVAL = 5.seconds # time between each post notification
+      DEFAULT_CHECK_TIME    = 5.minutes # time between each reddit fetch
+
       def self.run(list)
+        self.log("EVENT: ReddWatch started.")
         list = self.get_list(list)
 
         reddit = Reddwatch::Feed::Reddit.new
         notifier = Reddwatch::Notifier::LibNotify.new
 
         feed   = reddit.fetch(list.join('+'))
-        last_checked = 0
 
+        # On startup show the newest 5 posts
+        feed.take(5).each do |post|
+          msg = reddit.create_message(post)
+          notifier.send(msg)
+          sleep(DEFAULT_WAIT_INTERVAL)
+        end
+
+        last_checked = Time.now.utc.to_i
+
+        # Main loop
         loop do
           feed.each do |post|
             if post.created_utc > last_checked then
               msg = reddit.create_message(post)
               notifier.send(msg)
-              sleep(5)
+              sleep(DEFAULT_WAIT_INTERVAL)
             else
               break
             end
           end
           t = Thread.new { feed = reddit.fetch(list.join('+')) }
           last_checked = Time.now.utc.to_i
-          sleep(60*5) # sleep for 5 min
+          sleep(DEFAULT_CHECK_TIME)
           t.join
         end
       end
@@ -34,8 +63,11 @@ module Reddwatch
           pid = open('/tmp/reddwatch.pid', 'r').readline.strip.to_i
           Process.kill("KILL", pid)
           File.delete('/tmp/reddwatch.pid')
+          self.log("EVENT: ReddWatch stopped.")
+          self.status
         else
           puts "ERROR: ReddWatch is not running."
+          self.log("ERROR: ReddWatch is not running.")
         end
       end
 
@@ -47,18 +79,24 @@ module Reddwatch
         }
 
         msg[:content] = "Running..." if File.exists? '/tmp/reddwatch.pid'
-        
+
         Reddwatch::Notifier::LibNotify.new.send(msg)
       end
 
       def self.get_list(list)
         unless File.exists? "#{Reddwatch::DEFAULT_LIST_DIR}/#{list}"
-          $stderr.puts("ERROR: '#{list}' list does not exist.")
+          self.log("ERROR: '#{list}' list does not exist.")
           exit(1)
         end
 
         open("#{Reddwatch::DEFAULT_LIST_DIR}/#{list}", 'r').readlines.map do |line|
           line.strip
+        end
+      end
+
+      def self.log(msg)
+        File.open('/tmp/reddwatch.log', File::WRONLY|File::CREAT|File::APPEND) do |f|
+          f.puts "#{Time.now.utc.to_i}:: #{msg}"
         end
       end
     end
